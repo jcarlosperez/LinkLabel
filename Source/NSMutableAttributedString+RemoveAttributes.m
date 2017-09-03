1658ea4e9c7de5cd05b70219cc2b11bacc86d672
jcarlosperez <carlos@jcarlosperez.me>
2017-09-03 14:04:34 -0400

    fix header date and remove a swift file

--- Source/NSMutableAttributedString+RemoveAttributes.m
+++ Source/NSMutableAttributedString+RemoveAttributes.m
@@ -2,7 +2,7 @@
 //  NSMutableAttributedString+RemoveAttributes.h
 //  ObjC port of LinkLabel by Andrew Dart
 //
-//  Created by Juan Carlos Perez on 00/03/2017.
+//  Created by Juan Carlos Perez on 09/03/2017.
 //  Copyright (c) 2017 Juan Carlos Perez. All rights reserved.
 //
 
