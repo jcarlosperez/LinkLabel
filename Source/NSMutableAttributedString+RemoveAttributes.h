1a1addeb6824806c2e6a54e6f3a1ae38939a246d
jcarlosperez <carlos@jcarlosperez.me>
2017-09-03 14:03:31 -0400

    Port NSMutableAttributedString+RemoveAttributes

---
+++ Source/NSMutableAttributedString+RemoveAttributes.h
@@ -0,0 +1,15 @@
+//
+//  NSMutableAttributedString+RemoveAttributes.h
+//  ObjC port of LinkLabel by Andrew Dart
+//
+//  Created by Juan Carlos Perez on 00/03/2017.
+//  Copyright (c) 2017 Juan Carlos Perez. All rights reserved.
+//
+
+@interface NSMutableAttributedString (RemoveAttributes)
+
+- (void)removeAttributes;
+
+- (void)removeAttributesInRange:(NSRange)range;
+
+@end
---
+++ Source/NSMutableAttributedString+RemoveAttributes.m
@@ -0,0 +1,24 @@
+//
+//  NSMutableAttributedString+RemoveAttributes.h
+//  ObjC port of LinkLabel by Andrew Dart
+//
+//  Created by Juan Carlos Perez on 00/03/2017.
+//  Copyright (c) 2017 Juan Carlos Perez. All rights reserved.
+//
+
+@implementation NSMutableAttributedString (RemoveAttributes)
+
+- (void)removeAttributes {
+  NSRange range = NSMakeRange(0, self.length);
+  [self removeAttributesInRange:range];
+}
+
+- (void)removeAttributesInRange:(NSRange)range {
+  [string enumerateAttributesInRange:range options:kNilOptions usingBlock:^(NSDictionary<NSAttributedStringKey,id> * _Nonnull attrs, NSRange range, BOOL * _Nonnull stop) {
+    [attrs enumerateKeysAndObjectsUsingBlock:^(NSString *  _Nonnull key, NSAttributedStringKey  _Nonnull obj, BOOL * _Nonnull stop) {
+        [self removeAttribute:obj range:range];
+    }];
+  }];
+}
+
+@end
